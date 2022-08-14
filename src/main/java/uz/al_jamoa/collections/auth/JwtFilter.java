dfdb9e9bb9e38fea8fcc56c7fd1051117406bded
Bakhromjon Khasanboyev <xbakhromjon@gmail.com>
2022-08-14 18:54:02 +0500

    do-filter

--- src/main/java/uz/al_jamoa/collections/auth/JwtFilter.java
+++ src/main/java/uz/al_jamoa/collections/auth/JwtFilter.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 import uz.al_jamoa.exception.UniversalException;
 
@@ -17,14 +18,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Key;
+import java.util.Objects;
 
 @Configuration
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-
+    private final AuthService authService;
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        if (token != null && token.startsWith("Bearer")) {
+            token = token.substring(7);
+            if (jwtService.validationToken(token)) {
+                String username = jwtService.getUsername(token);
+                UserDetails userDetails = authService.loadUserByUsername(username);
+                if (!Objects.isNull(userDetails.getPassword())) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        }
         filterChain.doFilter(request, response);
     }
 
--- src/main/java/uz/al_jamoa/config/DataLoader.java
+++ src/main/java/uz/al_jamoa/config/DataLoader.java
@@ -1,6 +1,7 @@
 package uz.al_jamoa.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,9 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final AdminRepository adminRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Value(value = "${spring.jpa.hibernate.ddl-auto}")
+    private String ddl;
+
+
     @Override
     public void run(String... args) throws Exception {
-        Admin admin = new Admin("Bakhromjon", "Khasanboyev", "bakhromjon.com", passwordEncoder.encode("bakhromjon123"));
-        adminRepository.save(admin);
+        if (ddl.equals("create") || ddl.equals("create-drop")) {
+            Admin admin = new Admin("Bakhromjon", "Khasanboyev", "bakhromjon.com", passwordEncoder.encode("bakhromjon123"));
+            adminRepository.save(admin);
+        }
     }
 }
